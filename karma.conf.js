7ebaa61f93f6d4330029e66aa3c49a9ffc9d1aac
unknown <U62652@ust-global.com>
2021-02-14 15:52:07 +0530

    Initial Commit

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/AdityaEnterprise'),
+      dir: require('path').join(__dirname, './coverage/AdityaInventory'),
       subdir: '.',
       reporters: [
         { type: 'html' },

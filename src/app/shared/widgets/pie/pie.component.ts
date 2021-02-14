d42ecc2f5b2e095bd3fc724c766bfc1b31be2128
unknown <U62652@ust-global.com>
2021-02-14 16:22:16 +0530

    Initial Commit

--- src/app/shared/widgets/pie/pie.component.ts
+++ src/app/shared/widgets/pie/pie.component.ts
@@ -1,72 +1,72 @@
 import { Component, OnInit, Input } from '@angular/core';
 
-// import * as Highcharts from 'highcharts';
+import * as Highcharts from 'highcharts';
 
-// import HC_exporting from 'highcharts/modules/exporting';
+import HC_exporting from 'highcharts/modules/exporting';
 
 @Component({
   selector: 'app-widget-pie',
   templateUrl: './pie.component.html',
   styleUrls: ['./pie.component.scss']
 })
-export class PieComponent {
+export class PieComponent implements OnInit {
     
-  //   @Input() public pie;
+    @Input() public pie;
 
-  // Highcharts = Highcharts;
-  // chartOptions: {};
-  // constructor() { }
+  Highcharts = Highcharts;
+  chartOptions: {};
+  constructor() { }
 
-  // ngOnInit(): void {
-  //   this.chartOptions = 
-  //   {
-  //     chart: {
-  //         plotBackgroundColor: null,
-  //         plotBorderWidth: null,
-  //         plotShadow: false,
-  //         type: 'pie'
-  //     },
-  //     title: {
-  //         text: 'Randon data'
-  //     },
-  //     tooltip: {
-  //         pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-  //     },
-  //     accessibility: {
-  //         point: {
-  //             valueSuffix: '%'
-  //         }
-  //     },
-  //     plotOptions: {
-  //         pie: {
-  //             allowPointSelect: true,
-  //             cursor: 'pointer',
-  //             dataLabels: {
-  //                 enabled: true,
-  //                 format: '<b>{point.name}</b>: {point.percentage:.1f} %'
-  //             }
-  //         }
-  //     },
-  //     exporting: {
-  //       enabled: true
-  //     },
-  //     credits: {
-  //       enabled: false
-  //     },
-  //     series: [{
-  //         name: 'Brands',
-  //         colorByPoint: true,
-  //         data: this.pie
-  //     }]
-  // }
-  //   HC_exporting(Highcharts);
+  ngOnInit(): void {
+    this.chartOptions = 
+    {
+      chart: {
+          plotBackgroundColor: null,
+          plotBorderWidth: null,
+          plotShadow: false,
+          type: 'pie'
+      },
+      title: {
+          text: 'Randon data'
+      },
+      tooltip: {
+          pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+      },
+      accessibility: {
+          point: {
+              valueSuffix: '%'
+          }
+      },
+      plotOptions: {
+          pie: {
+              allowPointSelect: true,
+              cursor: 'pointer',
+              dataLabels: {
+                  enabled: true,
+                  format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+              }
+          }
+      },
+      exporting: {
+        enabled: true
+      },
+      credits: {
+        enabled: false
+      },
+      series: [{
+          name: 'Brands',
+          colorByPoint: true,
+          data: this.pie
+      }]
+  }
+    HC_exporting(Highcharts);
 
-  //   setTimeout(() => {
-  //     window.dispatchEvent(
-  //       new Event('resize')
-  //     );
-  //   }, 300);
+    setTimeout(() => {
+      window.dispatchEvent(
+        new Event('resize')
+      );
+    }, 300);
 
-  // }
+  }
 
 }
